697dec8b5d57004e650728cdd4340eed0e28605b
druz <druz@localhost.localdomain>
2020-10-03 22:44:13 -0400

    new

--- psets/pset6/mario.py
+++
@@ -1,8 +0,0 @@
-height = int(input('Height: '))
-counter = 0
-for i in range(height):
-    counter += 1
-    space_counter = height-counter
-    print(' '*space_counter, end='')
-    print('#'*counter)
-
