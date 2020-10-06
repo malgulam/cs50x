43ff5d2c3d2f73a3572129bf25d35e58e71abc9e
Agulam Chigabatia <freedom@pop-os.localdomain>
2020-10-06 20:25:54 +0000

    Better dictionary.c

--- psets/pset5/speller/dictionary.c
+++ psets/pset5/speller/dictionary.c
@@ -1,13 +1,13 @@
 // Implements a dictionary's functionality
-
-#include <stdbool.h>
-#include <ctype.h>
-#include <string.h>
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <strings.h>
 #include "dictionary.h"
+#include <ctype.h>
 
-#define HASTABLE_SIZE 1000
 // Represents a node in a hash table
 typedef struct node
 {
@@ -17,102 +17,139 @@ typedef struct node
 node;
 
 // Number of buckets in hash table
-const unsigned int N = 1;
+const unsigned int N = 456976;
+
+//number of counted nodes
+unsigned int count = 0;
 
 // Hash table
-node *hashtable[HASTABLE_SIZE];
+node *table[N];
 
-unsigned int num_words = 0;
-bool is_loaded_dict = false;
-//// Hashes word to a number
-unsigned int hash(const char* word)
-{
-    int hash = 0;
-    int n = 0;
-    for (int i =0 ; word[i] != '\0'; i++)
-    {
-        if (isalpha(word[i]))
-            n =  word[i] - 'a' +1;
-        else
-            n = 27;
-        hash - ((hash << 3) + n) % HASTABLE_SIZE;
+//set all pointers to null
+void set_null(){
+    for(int i = 0;i < N; i++){
+        table[i] = NULL;
     }
-    return hash;
 }
-//// Returns true if word is in dictionary else false
-bool check(const char* word)
-{
-    char check_word[strlen(word)];
-    strcpy(check_word, word);
-    for (int i = 0; check_word[i] != '\0'; i++)
-    {
-        check_word[i] = tolower(check_word[i]);
+//makes strings lower case
+void makelower(char *str){
+    for(int i = 0;str[i]; i++){
+  str[i] = tolower(str[i]);
     }
-    int index = hash(check_word);
-    if(hashtable[index] != NULL)
-    {
-        for (node* nodeptr = hashtable[index]; nodeptr != NULL; nodeptr = nodeptr->next)
-        if (strcmp(nodeptr->word, check_word) == 0)
-            return true;
-    }
-    return false;
 }
-//// Loads dictionary into memory, returning true if successful else false
-bool load(const char* dictionary)
-{
-    FILE *infile = fopen(dictionary, "r");
-    if (infile == NULL)
-        return false;
-    for (int i = 0; i < HASTABLE_SIZE; i++)
-        hashtable[i] = NULL;
-    char word[LENGTH + 1];
-    node* new_nodeptr;
-    while (fscanf(infile, "%s", word) != EOF)
-    {
-        num_words ++;
-        do
-        {
-            new_nodeptr = malloc(sizeof(node));
-            if (new_nodeptr == NULL)
-                free(new_nodeptr);
-        }
-        while(new_nodeptr == NULL);
-        strcpy(new_nodeptr->word, word);
-        int index = hash(word);
-        new_nodeptr-> next = hashtable[index];
-        hashtable[index] = new_nodeptr;
 
-    }
-    fclose(infile);
-    is_loaded_dict = true;
-    return true;
+// Returns true if word is in dictionary else false
+bool check(const char *word)
+{
+    //create temp variable and store word in it
+    /*This is because hash table works with ascii values and uppercase and lower case
+    do not have the same values so produce different results in hash function*/
+    char hi[LENGTH];
+    strcpy(hi,word);
+    makelower(hi);
+
+   //Allocate memory for temp node
+   unsigned int hash_value = hash(hi);
+   node *fake;
+
+   //temp points to first thing list head is pointing at
+   if(table[hash_value] == NULL) return false;
+   fake = table[hash_value];
+
+   //while the two are not matching check if there is no address in next to go to
+   while(strcmp(hi,fake->word) != 0){
+       if(fake->next == NULL) return false;
+       else fake = fake->next;
+   }
+   return true;
 }
-//// Returns number of words in dictionary if loaded else 0 if not yet loaded
+
+// Hashes word to a number
+unsigned int hash(const char *word)
+{
+     unsigned int length = strlen(word);
+     unsigned int  hash_value = 0;
+     for(int j = 0; j < length; j++){
+         hash_value += word[j];
+         hash_value *= LENGTH;
+         hash_value = (hash_value * word[j]) % N;
+     }
+     return hash_value;
+}
+
+
+
+// Loads dictionary into memory, returning true if successful else false
+bool load(const char *dictionary)
+{
+
+    //open dictionary file
+   FILE *yeah = fopen(dictionary,"r");
+       if (yeah == NULL) return false;
+
+       //create buffer to put strings in
+       char *buffer = (char *)malloc(sizeof(char) * LENGTH);
+       if(buffer == NULL){
+           fclose(yeah);
+           return false;
+       }
+
+       //set all pointers to NULL in table
+       set_null();
+
+       //add words and put new nodes in hash table
+       while(fscanf(yeah, "%s",buffer) != EOF){
+           //create node for that word
+           node *tmp =(node *) malloc(sizeof(node));
+           if(tmp == NULL){
+               fclose(yeah);
+               free(buffer);
+               return false;
+           }
+           tmp->next = NULL;
+           //make all words in the loaded dictionary lower case
+           makelower(buffer);
+           strcpy(tmp->word,buffer);
+
+           //get hash value
+          unsigned int hash_value = hash(buffer);
+
+           //check the situation of the node and how to fit it into table
+           if(table[hash_value] == NULL){
+               table[hash_value] = tmp;
+
+           }
+           else{
+               tmp->next = table[hash_value];
+               table[hash_value] = tmp;
+           }
+           count++;
+
+       }
+       fclose(yeah);
+       free(buffer);
+       return true;
+}
+
+// Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
-    if(is_loaded_dict)
-        return 0;
-    return num_words;
+     return count;
 }
-//// Unloads dictionary from memory, returning true if successful else false
+//function that uses recursion to delete nodes
+void deletenodes(node *temp){
+    if(temp==NULL)return;
+    else{
+        deletenodes(temp->next);
+        free(temp);
+    }
+}
+// Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
-    if(!is_loaded_dict)
-        return false;
-    for(int i = 0; i < HASTABLE_SIZE; i++)
-    {
-        if (hashtable[i] != NULL)
-        {
-            node* ptr = hashtable[i];
-            while (ptr != NULL)
-            {
-                node* preptr = ptr;
-                ptr = ptr-> next;
-                free(preptr);
-            }
-        }
-
-    }
-    return true;
+   for(int i = 0;i<N;i++){
+       deletenodes(table[i]);
+   }
+   return true;
 }
 
